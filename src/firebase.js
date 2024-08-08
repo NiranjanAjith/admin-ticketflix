313407718f971c7a4d37fac0558751a025fe5126
NiranjanAjith <140307778+NiranjanAjith@users.noreply.github.com>
2024-08-09 01:31:56 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAmfMeIrGF47znFqNbsgENmzlA6i4iuG74",
-    authDomain: "movie-campaign.firebaseapp.com",
-    projectId: "movie-campaign",
-    storageBucket: "movie-campaign.appspot.com",
-    messagingSenderId: "508614699542",
-    appId: "1:508614699542:web:569c5a6cc69f62facee337",
-    measurementId: "G-WSY57ZKPSE"
+    apiKey: "APIKEY",
+    authDomain: "DOMAIN",
+    projectId: "PROJECT",
+    storageBucket: "PROJECT_STORAGE",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "MEASUREMENT_ID"
   };
 
 // Initialize Firebase
