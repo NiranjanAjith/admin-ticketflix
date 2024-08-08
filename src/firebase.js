cad7ad92df9beb04f335094cb5f44e31d66ef437
NiranjanAjith <140307778+NiranjanAjith@users.noreply.github.com>
2024-08-09 01:39:12 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "APIKEY",
+    authDomain: "DOMAIN",
+    projectId: "PROJECT",
+    storageBucket: "BUCKET",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "MEASUREMENT_ID"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get Firebase services
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
