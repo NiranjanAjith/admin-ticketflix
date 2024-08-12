53e8e62fedcadda980e4ca2c7ce74d175b2f419c
NiranjanAjith <NiranjanAjithKumar@gmail.com>
2024-08-12 13:13:42 +0530

    tailwind ~aadhil

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './styles.css';
 
+
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import { auth } from './firebase';
